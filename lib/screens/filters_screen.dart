61d35ba2bf0a73ab4d013ff45c3bd19a7a887d52
mutohiroshi <mutohiroshi@team-lab.com>
2020-08-11 17:22:36 +0900

    add: add navigation function

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class FiltersScreen extends StatelessWidget {
+  static const routeName = '/filters';
+
   @override
   Widget build(BuildContext context) {
     return Center(
